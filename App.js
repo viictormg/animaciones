93c2a73264f7b9068719b531e129e522b1f6835c
desarrollo54 <desarrollo5@aveonline.co>
2020-08-29 17:26:08 -0500

    first commit

---
+++ App.js
@@ -0,0 +1,35 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+
+const App = () => {
+  return (
+    <>
+      <View style={styles.contenido}>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  contenido :  {
+    marginTop : 100
+  }
+})
+
+
+export default App;
